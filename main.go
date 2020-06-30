59d3d6d2c51a7e3fb7a1f9b4d0080ed66fd9c45c
Darko Donnie <donni3thedarko@gmail.com>
2020-06-30 15:41:24 +0000

    first module exercises

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func print_name(name string) {
+  fmt.Println(name)
+
+}
+
 func main() {
   fmt.Println("Hello World")
+  print_name("Isaac")
+  fmt.Print("What happens without ln?")
+  //fmt.Println('What happens with single quotes?')
+  fmt.Println("Move package main to the bottom is a stupid exercise")
+
 }
+
