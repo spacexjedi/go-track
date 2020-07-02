66042f1ab65976aa1c3b85fa240e765978b0c8f6
Darko Donnie <donni3thedarko@gmail.com>
2020-07-02 20:17:36 +0000

    package scope, go docs

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
+
 import "fmt"
+import f "fmt"
+import "runtime"
 
 func print_name(name string) {
   fmt.Println(name)
@@ -13,6 +16,17 @@ func main() {
   fmt.Print("What happens without ln?")
   //fmt.Println('What happens with single quotes?')
   fmt.Println("Move package main to the bottom is a stupid exercise")
+  f.Println("Alias to packages")
+  f.Print("semicolon");
+  f.Print("o");
+  // the most stupid thing you can do 
+  //"Hello"
+  fmt.Println("Hello!" + "!" + "!" + "!" + "?")
+  fmt.Println(runtime.Version())
+//  fmt.Println("hello")
+//	fmt.Println("how")
+//	fmt.Println("are")
+//	fmt.Println("you")
 
 }
 
